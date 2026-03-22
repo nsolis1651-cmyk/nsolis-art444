adcea7da5fe31f45fce3df285f7fe4104613400a
Nadia Solis <nsolis1651@sdsu.edu>
2026-03-21 22:21:10 -0700

    3/21
    Update 3/21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // ALL YOU JS GOES HERE
+
+const burger = document.querySelector('div.burger');
+burger.addEventListener('click', classlistmenu).classList;
+
+function classlistmenu(){
+    console.log('Classlist Shows');
+    burger.toggle('classlistmenu');
+}
