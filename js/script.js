8e090d3ea3ecbd23110f5674fb0c8087b1ec329a
Nadia Solis <nsolis1651@sdsu.edu>
2026-04-16 22:15:34 -0700

    4/16
    Dark Mode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ALL YOU JS GOES HERE
-
+//burger toggle
 const burger = document.querySelector('div.burger');
 const classlistmenu = document.querySelector('.classlist').classList;
 
@@ -12,4 +12,38 @@ burger.addEventListener('click', revealclasslistmenu);
 
 
 
+// ----------------------------------------------------------
+// dark mode toggle//
 
+
+const svg = document.querySelector('.burger svg');
+const btn = document.querySelector('#theme-toggle');
+
+
+function updateSvgColor() {
+  if (document.body.classList.contains('dark-mode')) {
+    svg.setAttribute('fill', 'white');
+  } else {
+    svg.setAttribute('fill', 'black');
+  }
+}
+
+function applyThemeByTime() {
+  const currenthour = new Date().getHours();
+  if (currenthour >= 18 || currenthour < 6) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+  // Call helper to set color on load
+  updateSvgColor();
+}
+
+
+applyThemeByTime();
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  updateSvgColor();
+});
